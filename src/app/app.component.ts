a099c997029b50ac2a9045cbcb69b280e4a083e6
debeelzebub <sruthysurendrants@gamil.com>
2024-01-29 14:06:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+import { DeviceDetectorService } from "ngx-device-detector";
+import { AES } from "crypto-js";
+import { enc } from "crypto-js";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sidrateams';
+  useragentAPI:any = "https://api.apicagent.com/?ua="
+
+  apiUrl:any = 'https://example.com/device-detection-api';
+
+  constructor(private http: HttpClient,private deviceinfo:DeviceDetectorService){
+      
+    console.log(deviceinfo.getDeviceInfo()) 
+
+    
+  
+  }
+
+  ngOnInit(): void {
+    const userAgent =window.navigator.userAgent
+
+      this.http.get(this.useragentAPI + userAgent).subscribe((d:any)=>{
+          console.log(d)
+
+      
+      
+      
+      })
+     
+
+      // encryptdata
+      let data = 'fasalgafoork@gmail.com'
+      console.log(data)
+      const encrypteddata =AES.encrypt(JSON.stringify(data), 'data_key').toString();
+      console.log(encrypteddata)
+
+      // decryptdata
+      const decrypteddata = AES.decrypt(encrypteddata, 'data_key')
+      console.log(decrypteddata.toString(enc.Utf8))
+
+
+      
+
+     
+
+      
+  }
 }
---
+++ src/polyfills.ts
@@ -0,0 +1,7 @@
+/***************************************************************************************************
+* Zone JS is required by Angular itself.
+*/
+(window as any).__Zone_enable_cross_context_check = true;
+
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+// ... other polyfills
